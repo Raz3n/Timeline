c8ef498ce844ccc050cc85277071db0c579aa921
Morgaine <morgainedv@gmail.com>
2019-12-09 10:50:12 +0000

    adds html to template to playingcard.vue

--- client/src/components/PlayingCard.vue
+++ client/src/components/PlayingCard.vue
@@ -1,5 +1,9 @@
 <template lang="html">
-  
+  <article class="playing-card">
+    <p> Short Title:</p>
+    <img src="blank" alt="Title"/>
+    <p class="hidden">Year: </p>  
+  </article>
 </template>
 
 <script>
