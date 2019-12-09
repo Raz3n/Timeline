8de30c5edcac4933728ee8341d673a5963f7cf4b
Morgaine <morgainedv@gmail.com>
2019-12-09 10:28:29 +0000

    adds template structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,22 @@
 <template lang="html">
-
+  <div id="app">
+    <nav>
+      <section id="instructions" class="nav">
+        Instructions
+      </section>
+      <section id="play" class="nav">
+        Play
+      </section>
+      <section id="change-rules" class="nav">
+        Change Rules
+      </section>
+    </nav>
+    <section id="play-area-container">
+      Board Area
+      Player Hand
+    </section>
+    <footer> Scores </footer>
+  </div>
 </template>
 
 <script>
