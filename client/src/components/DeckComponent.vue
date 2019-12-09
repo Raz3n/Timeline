a12c71aa04d65ac97e8e1092e4a07f857651bf76
Morgaine <morgainedv@gmail.com>
2019-12-09 10:45:15 +0000

    imports playing card component to deckcomponent.vue

--- client/src/components/DeckComponent.vue
+++ client/src/components/DeckComponent.vue
@@ -1,10 +1,20 @@
 <template lang="html">
+    <aside id="deck">
+      This is where the deck is
+    </aside>
+
+
 
 </template>
 
 <script>
+import PlayingCard from './PlayingCard.vue'
 export default {
-  name: 'deck-component'
+  name: 'deck-component',
+  props: ['deckArray'],
+  components: {
+    'playing-card': PlayingCard
+  }
 }
 </script>
 
