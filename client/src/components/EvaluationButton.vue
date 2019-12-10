73cf86d96c2ddda8d83463b62f110c7faf4625be
Morgaine <morgainedv@gmail.com>
2019-12-10 11:21:10 +0000

    adds comments to evaluation function for clarity

--- client/src/components/EvaluationButton.vue
+++ client/src/components/EvaluationButton.vue
@@ -22,40 +22,29 @@ export default {
       this.forEach(card (value, index, array) => {
         const leftCardIndex = index -1;
         const rightCardIndex = index +1;
+        //left card index is the index of array minus 1
+        //right card index is the index of array plus 1
 
+        //define the leftcard as the card at the left card index position
+        //define the right card as the card at the right card index position
         const leftCard = array[leftCardIndex];
         const rightCard = array[rightCardIndex];
 
+
+        //if there is a left card and its year is more than the card year return false
         if (leftCard && leftCard.year > card.year) {
           return false
         }
-        else if {
+        else if {     //if there is a right card and its year is more than the card year return false
           (rightCard && rightCard.year < card.year) {
             return false
           }
 
-          else {
+          else {        //else card is correct and function ends
             return true
           }
         }
       })
-
-
-      //
-      // if  (left card year value > played card )
-      // return false
-      //
-      // else if (compare to year value of right card
-      // if right card year value is < played card )
-      // return false
-      //
-      // else
-      //
-      // true
-
-
-
-
     }
   }
 
