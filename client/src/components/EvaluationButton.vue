adb18e9da1d4d87d6ec3829e0c07fabceb90440a
Nelson <nbritob@hotmail.com>
2019-12-10 17:09:39 +0000

    fixes evaluation method

--- client/src/components/EvaluationButton.vue
+++ client/src/components/EvaluationButton.vue
@@ -19,39 +19,49 @@ export default {
     evaluateCard () {
       // find index of played card
       // compare to year of left card
-      let failCounter = 0;
-      this.cardsInPlay.forEach(function(card, index, array){
-        const leftCardIndex = index -1;
-        const rightCardIndex = index +1;
+      // let failCounter = 0;
+      // this.cardsInPlay.forEach(function(card, index, array)
+
+
+      for (let i = 0; i < this.cardsInPlay.length; i++)
+       {
+         // debugger
+        const leftCard = this.cardsInPlay[i -1];
+        const rightCard = this.cardsInPlay[i +1];
+        const card = this.cardsInPlay[i];
 
         //left card index is the index of array minus 1
         //right card index is the index of array plus 1
 
         //define the leftcard as the card at the left card index position
         //define the right card as the card at the right card index position
-        const leftCard = array[leftCardIndex];
-        const rightCard = array[rightCardIndex];
+        // const leftCard = this.cardsInPlay[leftCardIndex];
+        // const rightCard = this.cardsInPlay[rightCardIndex];
 
         //if there is a left card and its year is more than the card year return false
-        if (leftCard && leftCard.year > card.year) {
-          failCounter++
+        if (leftCard && parseInt(leftCard.year) > parseInt(card.year)) {
+          console.log(i);
+          console.log("incorrect - left card higher")
         }
         //if there is a right card and its year is more than the card year return false
 
-        else if (rightCard && rightCard.year < card.year) {
-          failCounter++
+        else if (rightCard && parseInt(rightCard.year) < parseInt(card.year)) {
+          console.log(i);
+          console.log("incorrect - right card lower")
           }
 
-          else {        //else card is correct and function ends
+          else {
+            console.log(i);
+            console.log("correct")      //else card is correct and function ends
 
           }
         }
-      );
-      if (failCounter > 0) {
-        console.log('loser')
-      } else (
-        console.log('congrats')
-      )
+
+      // if (failCounter > 0) {
+      //   console.log('loser')
+      // } else (
+      //   console.log('congrats')
+      // )
 
     }
   }
