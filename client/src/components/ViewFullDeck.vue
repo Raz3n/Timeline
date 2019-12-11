761f05136e474a77dccf423922308fd6f8be6a06
Ryan <ryanfrost2015@gmail.com>
2019-12-11 11:45:33 +0000

    Ryan accidentally worked in develop. Only altered ViewFullDeck component

--- client/src/components/ViewFullDeck.vue
+++ client/src/components/ViewFullDeck.vue
@@ -1,97 +1,11 @@
 <template lang="html">
-  <section id="view-full-deck-wrapper">
-<button v-on:click="sortByYear()" type="button">Sort by year</button>
-    <!-- Insert Components Needed To Render Entire Deck Here  -->
-<ul id="flex">
-  <!-- Want the UL to be flex -->
-  <li v-for="(card, index) in currentDeck"  >
-    <playing-card :card='card':key="index" />
-    <!-- <p>{{ card.title}}</p> -->
-    <p>{{ card.year}}</p>
-    <p>{{ card.description }}</p>
-  </li>
-</ul>
 
-    <!-- <playing-card v-for="(card, index) in currentDeck" :key="index" :card="card"/>
-    <div v-for="(card, index) in currentDeck" :key="index"> -->
-
-  </section>
 </template>
 
 <script>
-import PlayingCard from './PlayingCard.vue'
-
 export default {
-  name: 'view-full-deck',
-
-  data(){
-    return {
-      currentDeck: []
-    }
-  },
-
-  components: {
-    'playing-card': PlayingCard
-  },
-
-  mounted() {
-    // this.gameSetup();
-    this.deckSetup();
-  },
-
-  methods: {
-    deckSetup(){
-      // get all the data
-      return fetch('http://localhost:3000/api/cards')
-
-      .then(res => res.json())
-
-      .then(cards => this.currentDeck = cards)
-
-
-      // understand the data
-      // return these cards as a full deck called 'this.currentDeck'
-    },
-    sortByYear() {
-      copyOfCurrentDeck = this.currentDeck.map((card) => {return card});
-      chronologicallySortedCards
-
-
-      this.currentDeck.sort(function(a, b) {
-        if(a.year < b.year) { return -1; }
-        if(a.year > b.year) { return -1; }
-        return 0;
-      })
-    }
-
-    copyOfUsers = users.map((user) => {return user});
-sortedUsers = copyOfUsers.sort(function(a, b){
-    if(a.year < b.year) { return -1; }
-    if(a.year > b.year) { return 1; }
-    return 0;
-})
-
-console.log('u',users);
-console.log('c',copyOfUsers);
-console.log('sorted',sortedUsers);
-
-  }
 }
 </script>
 
 <style lang="css" scoped>
-
-
-#view-full-deck-wrapper{
-  grid-row: 2 / 3;
-  grid-column: 1 / 4;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding: 1em;
-  align-items: center;
-  min-height: 35em;
-}
-
-
 </style>
